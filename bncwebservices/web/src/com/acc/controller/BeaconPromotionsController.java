368bed194ab1c613501e70df28214d00f3166548
swarnimagupta <swarnima.gupta@accenture.com>
2015-01-28 16:31:40 +0530

    Beacon Promotions webservice code

--- bncwebservices/web/src/com/acc/controller/BeaconPromotionsController.java
+++ bncwebservices/web/src/com/acc/controller/BeaconPromotionsController.java
@@ -3,13 +3,9 @@
  */
 package com.acc.controller;
 
-import de.hybris.platform.commercefacades.product.data.PromotionData;
+import java.util.Collections;
 
-import java.util.List;
-
-import javax.jws.WebParam;
-import javax.xml.ws.Holder;
-
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.acc.data.BeaconData;
+import com.acc.data.BeaconPromotionData;
 import com.acc.services.BeaconPromotionsService;
 
 
@@ -37,15 +34,16 @@ public class BeaconPromotionsController extends BaseController
 
 	@RequestMapping(value = "/{beaconId}/{majorId}/{minorId}", method = RequestMethod.GET)
 	@ResponseBody
-	public void getPromotionsForBeaconId(@PathVariable final String beaconId, @PathVariable final String majorId,
-			@PathVariable final String minorId,
-			@WebParam(name = "welcomeMessage", mode = WebParam.Mode.OUT) final Holder<String> welcomeMessage,
-			@WebParam(name = "promotions", mode = WebParam.Mode.OUT) final Holder<List<PromotionData>> promotions)
+	public BeaconPromotionData getPromotionsForBeaconId(@PathVariable final String beaconId, @PathVariable final String majorId,
+			@PathVariable final String minorId)
 	{
 		LOG.info("in getPromotionsForBeaconId GET request method :::::::beaconId is :::::" + beaconId + "::::::::majorId is :::::"
 				+ majorId + "AND minorId::::" + minorId);
 		final BeaconData beacon = beaconPromotionsService.getBeaconById(beaconId, majorId, minorId);
-		welcomeMessage.value = beacon.getWelcomeMessage();
-		promotions.value = beacon.getPromotions();
+		final BeaconPromotionData beaconPromotionsData = new BeaconPromotionData();
+		beaconPromotionsData.setWelcomeMessage(beacon.getWelcomeMessage());
+		beaconPromotionsData.setPromotions(CollectionUtils.isNotEmpty(beacon.getPromotions()) ? beacon.getPromotions()
+				: Collections.EMPTY_LIST);
+		return beaconPromotionsData;
 	}
 }
