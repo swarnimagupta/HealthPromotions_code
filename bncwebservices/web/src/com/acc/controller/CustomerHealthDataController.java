ff7f9bb552dae74a386a206120b71954c8d930b9
swarnimagupta <swarnima.gupta@accenture.com>
2015-01-28 15:14:44 +0530

    Customer health data webservice controller

--- bncwebservices/web/src/com/acc/controller/CustomerHealthDataController.java
+++ bncwebservices/web/src/com/acc/controller/CustomerHealthDataController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.acc.controller;
 
@@ -16,25 +16,26 @@ import com.acc.services.CustomerHealthDataService;
 
 /**
  * @author swarnima.gupta
- * 
+ *
  */
 @Controller
 @RequestMapping(value = "/v1/{baseSiteId}/CustomerHealthData")
-public class HealthPromotionsDataController extends BaseController
+public class CustomerHealthDataController extends BaseController
 {
-	private static final Logger LOG = Logger.getLogger(CSRController.class);
+	private static final Logger LOG = Logger.getLogger(CustomerHealthDataController.class);
 	@Autowired
 	private CustomerHealthDataService customerHealthDataService;
 
 	@RequestMapping(value = "/{customerId}/{heartBeatRate}/{bloodPressure}/{milesRun}/{caloriesBurned}/{timeTaken}/{age}", method = RequestMethod.GET)
 	@ResponseBody
-	public String saveCustomerHeathData(@PathVariable final String customerId, @PathVariable final Integer heartBeatRate,
-			@PathVariable final Integer bloodPressure, @PathVariable final Integer milesRun,
-			@PathVariable final Integer caloriesBurned, @PathVariable final Integer timeTaken, @PathVariable final Integer age)
+	public String saveCustomerHeathData(@PathVariable final String customerId, @PathVariable final String heartBeatRate,
+			@PathVariable final String bloodPressure, @PathVariable final String milesRun,
+			@PathVariable final String caloriesBurned, @PathVariable final String timeTaken, @PathVariable final String age)
 	{
 		LOG.info("in saveCustomerHeathData GET request method :::::::customerId is :::::" + customerId
 				+ "::::::::heartBeatRate is :::::" + heartBeatRate + "AND age::::" + age);
-		return customerHealthDataService.saveCustomerHealthData(customerId, heartBeatRate, bloodPressure, milesRun, caloriesBurned,
-				timeTaken, age);
+		return customerHealthDataService.saveCustomerHealthData(customerId, Integer.valueOf(heartBeatRate),
+				Integer.valueOf(bloodPressure), Integer.valueOf(milesRun), Integer.valueOf(caloriesBurned),
+				Integer.valueOf(timeTaken), Integer.valueOf(age));
 	}
 }
