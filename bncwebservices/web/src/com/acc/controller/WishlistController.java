75b917149050f0d821b07c142d87a83e8c648a96
swarnimagupta <swarnima.gupta@accenture.com>
2015-03-03 11:42:21 +0530

    Webservice for add product to wishlist functionality

--- bncwebservices/web/src/com/acc/controller/WishlistController.java
+++ bncwebservices/web/src/com/acc/controller/WishlistController.java
@@ -6,20 +6,24 @@ package com.acc.controller;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.core.model.user.UserModel;
+import de.hybris.platform.product.ProductService;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 import de.hybris.platform.servicelayer.user.UserService;
 import de.hybris.platform.wishlist2.Wishlist2Service;
+import de.hybris.platform.wishlist2.enums.Wishlist2EntryPriority;
 import de.hybris.platform.wishlist2.model.Wishlist2EntryModel;
 import de.hybris.platform.wishlist2.model.Wishlist2Model;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
@@ -42,6 +46,7 @@ import com.acc.product.data.ProductDataList;
 @RequestMapping(value = "/v1/{baseSiteId}/wishlist")
 public class WishlistController extends BaseController
 {
+	private static final String PRODUCT_ID = "productId";
 	private static final String ADD_TO_WISHLIST_FUNCTIONALITY = "add to wishlist functionality";
 	private static final String WISHLIST = "wishlist";
 	private static final String UID = "uid";
@@ -54,12 +59,14 @@ public class WishlistController extends BaseController
 	private UserService userService;
 	@Autowired
 	private Converter<ProductModel, ProductData> productConverter;
+	@Autowired
+	private ProductService productService;
 
 	@RequestMapping(value = "/get", method = RequestMethod.POST)
 	@ResponseBody
 	public ProductDataList getWishlistProducts(final HttpServletRequest request) throws IOException, ParseException
 	{
-		LOG.info("::::::: in saveCustomerHeathData GET request method :::::::");
+		LOG.info("::::::: in getWishlistProducts POST request method :::::::");
 		final StringBuffer sbuf = getJsonBodyString(request);
 		LOG.info("::::::: json object string is :::::::" + sbuf);
 		final ProductDataList productDataList = new ProductDataList();
@@ -69,24 +76,75 @@ public class WishlistController extends BaseController
 			final JSONObject obj = (JSONObject) parser.parse(sbuf.toString());
 			final String uid = String.valueOf(obj.get(UID));
 			final UserModel userModel = userService.getUserForUID(uid);
-
-			if (wishlistService.hasDefaultWishlist(userModel))
+			final Wishlist2Model wishList = wishlistService.hasDefaultWishlist(userModel) ? wishlistService
+					.getDefaultWishlist(userModel) : wishlistService.createDefaultWishlist(userModel, WISHLIST,
+					ADD_TO_WISHLIST_FUNCTIONALITY);
+			if (CollectionUtils.isNotEmpty(wishList.getEntries()))
 			{
-				final Wishlist2Model wishLists = wishlistService.getDefaultWishlist(userModel);
-				final List<ProductData> productsData = new ArrayList<ProductData>();
-				final List<Wishlist2EntryModel> entries = wishLists.getEntries();
-				for (final Wishlist2EntryModel entry : entries)
-				{
-					productsData.add(productConverter.convert(entry.getProduct()));
-				}
-				productDataList.setProducts(productsData);
-			}
-			else
-			{
-				wishlistService.createDefaultWishlist(userModel, WISHLIST, ADD_TO_WISHLIST_FUNCTIONALITY);
+				productDataList.setProducts(getWishlistProductsList(wishList));
 			}
 		}
 		return productDataList;
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	@ResponseBody
+	public ProductDataList addWishlistProducts(final HttpServletRequest request) throws IOException, ParseException
+	{
+		LOG.info("::::::: in addWishlistProducts POST request method :::::::");
+		final StringBuffer sbuf = getJsonBodyString(request);
+		LOG.info("::::::: json object string is :::::::" + sbuf);
+		final ProductDataList productDataList = new ProductDataList();
+		if (StringUtils.isNotEmpty(sbuf.toString()))
+		{
+			final JSONParser parser = new JSONParser();
+			final JSONObject obj = (JSONObject) parser.parse(sbuf.toString());
+			final ProductModel productModel = productService.getProductForCode(String.valueOf(obj.get(PRODUCT_ID)));
+			final String uid = String.valueOf(obj.get(UID));
+			final UserModel userModel = userService.getUserForUID(uid);
+			Boolean flag = Boolean.FALSE;
+			final Boolean newWishlistFlag = wishlistService.hasDefaultWishlist(userModel) ? Boolean.FALSE : Boolean.TRUE;
+			final Wishlist2Model wishList = wishlistService.hasDefaultWishlist(userModel) ? wishlistService
+					.getDefaultWishlist(userModel) : wishlistService.createDefaultWishlist(userModel, WISHLIST,
+					ADD_TO_WISHLIST_FUNCTIONALITY);
+			if (CollectionUtils.isNotEmpty(wishList.getEntries()))
+			{
+				final List<Wishlist2EntryModel> entries = wishList.getEntries();
+				for (final Wishlist2EntryModel entry : entries)
+				{
+					if (entry.getProduct().getCode().equals(productModel.getCode()))
+					{
+						flag = Boolean.TRUE;
+						break;
+					}
+				}
+			}
+			if (!flag || newWishlistFlag)
+			{
+				final Wishlist2EntryModel productEntry = new Wishlist2EntryModel();
+				productEntry.setProduct(productModel);
+				productEntry.setPriority(Wishlist2EntryPriority.LOW);
+				productEntry.setAddedDate(new Date());
+				wishlistService.addWishlistEntry(wishList, productEntry);
+			}
+			productDataList.setProducts(getWishlistProductsList(wishList));
+		}
+		return productDataList;
+	}
+
+	/**
+	 * @param wishList
+	 * @return List<ProductData>
+	 */
+	private List<ProductData> getWishlistProductsList(final Wishlist2Model wishList)
+	{
+		final List<Wishlist2EntryModel> entries = wishList.getEntries();
+		final List<ProductData> productsData = new ArrayList<ProductData>();
+		for (final Wishlist2EntryModel entry : entries)
+		{
+			productsData.add(productConverter.convert(entry.getProduct()));
+		}
+		return productsData;
 	}
 
 	/**
