103a4b5ae972c22e8d469f8b32fc03f8b236f111
swarnimagupta <swarniima@gmail.com>
2015-04-08 14:48:01 +0530

    uninstallation addon for removal of health data

---
+++ uninstallation/healthdataremovaladdon/gensrc/com/acc/constants/GeneratedHealthdataremovaladdonConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Apr 8, 2015 10:46:16 AM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedHealthdataremovaladdonConstants
+{
+	public static final String EXTENSIONNAME = "healthdataremovaladdon";
+	
+	protected GeneratedHealthdataremovaladdonConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
