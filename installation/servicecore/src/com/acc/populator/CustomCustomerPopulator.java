7f2df9d6dd2611c4926aa8f3a61ec637c8cc3e02
swarnimagupta <swarniima@gmail.com>
2015-05-15 11:39:01 +0530

    Adding populator for customer's new attributes

---
+++ installation/servicecore/src/com/acc/populator/CustomCustomerPopulator.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.acc.populator;
+
+import de.hybris.platform.commercefacades.user.converters.populator.CustomerPopulator;
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+import de.hybris.platform.core.model.user.CustomerModel;
+
+
+/**
+ * @author swarnima.gupta
+ *
+ */
+public class CustomCustomerPopulator extends CustomerPopulator
+{
+
+	@Override
+	public void populate(final CustomerModel source, final CustomerData target)
+	{
+		super.populate(source, target);
+		target.setLatitudes(source.getLatitudes());
+		target.setLongitudes(source.getLongitudes());
+	}
+}
